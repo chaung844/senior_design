4d55896c8e2c464026cbf8071339c90e5a237c51
Tiep Tran <51280433+polskiTran@users.noreply.github.com>
2026-02-13 23:56:00 -0500

    ignore ruff unused import in alembic folder

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -6,13 +6,11 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
 from app.config import get_settings
-from app.models.base import Base  
-from app.models.user import User
-from app.models.statement import BankStatement, BankStatementLine
+from app.models.base import Base
 from app.models.receipt import Receipt
-
+from app.models.statement import BankStatement, BankStatementLine
+from app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
